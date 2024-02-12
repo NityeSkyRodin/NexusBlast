0e1007d2aa739a0ba0dc4e509e2c6ee5bdff32d5
Lars Meijerink <L.meijerink@student.han.nl>
2024-02-12 10:53:56 +0100

    startup

---
+++ desktop/src/com/nl/nexusblast/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.nl.nexusblast;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.nl.nexusblast.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("NexusBlast");
+		new Lwjgl3Application(new Main(), config);
+	}
+}
